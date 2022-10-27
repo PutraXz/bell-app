ef4f82f6786bece855a68f0eccf51927c5ae962d
DodoJowi <jowidodo40@gmail.com>
2022-10-27 16:31:10 +0700

    better

--- resources/views/bell.blade.php
+++ resources/views/bell.blade.php
@@ -18,7 +18,7 @@
         <div class="wrapper">
             <table class="table table-hover mt-5">
                 <thead>
-                    <th>No</th>
+                    <th>no</th>
                     <th>Jam</th>
                     <th>Senin</th>
                     <th>Selasa</th>
